bb622f9394b2586aca6f19788407327f507bdc17
Tatá <143547731+tataFaucz@users.noreply.github.com>
2025-06-03 14:40:13 -0300

    Update Sistema.java

--- projeto rede social/negocio/Sistema.java
+++ projeto rede social/negocio/Sistema.java
@@ -1,41 +1,47 @@
-package negocio;
+package negocios;
 
-import dados.Foto;
 import dados.Usuario;
-
+/*import dados.Foto;
+import dados.Recado;*/
 import java.util.ArrayList;
 import java.util.List;
 
 public class Sistema {
     private List<Usuario> usuarios;
-    private List<Foto> fotos;
 
-    public Sistema() {
-        this.usuarios = new ArrayList<>();
-        this.fotos = new ArrayList<>();
+        public Sistema() {
+            this.usuarios = new ArrayList<>();
+        }
+
+        public void adicionarUsuario(Usuario usuario) { usuarios.add(usuario); }
+        public Usuario buscarUsuario(String nome) {
+            for (Usuario u : usuarios) {
+                if (u.getNome().equalsIgnoreCase(nome)) 
+                return u;
+            }
+            return null;
+        }
+        public List<Usuario> listarUsuarios(){ 
+            return usuarios; 
+        }
+        public void adicionarAmg(Usuario usuario, Usuario amg) {
+        usuario.adicionarAmigo(amg);
     }
 
-    public void adicionarUsuario(Usuario usuario) {
-        usuarios.add(usuario);
+    public void removerAmg(Usuario usuario, Usuario amg) {
+        usuario.removerAmigo(amg);
     }
 
-    public void adicionarFoto(Foto foto) {
-        fotos.add(foto);
+    public List<Usuario> listarAmgs(Usuario usuario) {
+        return new ArrayList<>(usuario.getAmigos());
     }
-
-    public List<Usuario> listarUsuarios() {
-        return usuarios;
-    }
-
-    public List<Foto> listarFotos() {
-        return fotos;
-    }
-
-    @Override
-    public String toString() {
-        return "Sistema{" +
-                "usuarios=" + usuarios +
-                ", fotos=" + fotos +
-                '}';
+    public List<Usuario> listarNovosPerfis(Usuario usuario) {
+        List<Usuario> novos = new ArrayList<>();
+        for (Usuario u : usuarios) {
+            if (!usuario.getAmigos().contains(u) && !u.equals(usuario)) {
+                novos.add(u);
+            }
+        }
+        return novos;
     }
 }
