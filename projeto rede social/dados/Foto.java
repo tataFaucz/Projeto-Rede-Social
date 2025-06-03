213996e76b9f6341cb30016a8eae3a1568597d39
Tatá <143547731+tataFaucz@users.noreply.github.com>
2025-06-03 14:41:17 -0300

    Update Foto.java

--- projeto rede social/dados/Foto.java
+++ projeto rede social/dados/Foto.java
@@ -1,48 +1,21 @@
 package dados;
 
-import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Foto {
-    private String descricao;
-    private String caminhoArquivo;
-    private LocalDateTime dataPostagem;
+    private String caminho;
+    private String legenda;
+    private List<String> comentarios;
 
-    public Foto(String descricao, String caminhoArquivo, LocalDateTime dataPostagem) {
-        this.descricao = descricao;
-        this.caminhoArquivo = caminhoArquivo;
-        this.dataPostagem = dataPostagem;
+    public Foto(String caminho, String legenda) {
+        this.caminho = caminho;
+        this.legenda = legenda;
+        this.comentarios = new ArrayList<>();
     }
 
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public String getCaminhoArquivo() {
-        return caminhoArquivo;
-    }
-
-    public void setCaminhoArquivo(String caminhoArquivo) {
-        this.caminhoArquivo = caminhoArquivo;
-    }
-
-    public LocalDateTime getDataPostagem() {
-        return dataPostagem;
-    }
-
-    public void setDataPostagem(LocalDateTime dataPostagem) {
-        this.dataPostagem = dataPostagem;
-    }
-
-    @Override
-    public String toString() {
-        return "Foto{" +
-                "descricao='" + descricao + '\'' +
-                ", caminhoArquivo='" + caminhoArquivo + '\'' +
-                ", dataPostagem=" + dataPostagem +
-                '}';
-    }
+    public String getCaminho() { return caminho; }
+    public String getLegenda() { return legenda; }
+    public List<String> getComentarios() { return comentarios; }
+    public void adicionarComentario(String comentario) { comentarios.add(comentario); }
 }
