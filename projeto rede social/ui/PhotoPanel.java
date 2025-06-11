3ffbabacaacc36e1a41c9f29e72403b5f7ea4774
Tatá <143547731+tataFaucz@users.noreply.github.com>
2025-06-11 19:50:13 -0300

    Update PhotoPanel.java

--- projeto rede social/ui/PhotoPanel.java
+++ projeto rede social/ui/PhotoPanel.java
@@ -10,16 +10,29 @@ public class PhotoPanel extends JPanel {
     public PhotoPanel(Usuario usuario) {
         setLayout(new BorderLayout());
         JLabel label = new JLabel("Floguinho - Suas fotos:");
+        label.setHorizontalAlignment(SwingConstants.CENTER);
+        label.setFont(new Font("Arial", Font.BOLD, 18));
         add(label, BorderLayout.NORTH);
 
         JPanel fotosPanel = new JPanel();
-        fotosPanel.setLayout(new FlowLayout());
-        List<Foto> fotos = usuario.getFotos();
+        fotosPanel.setLayout(new FlowLayout(FlowLayout.LEFT, 10, 10));
+
+        List<Foto> fotos = usuario.getPublicacoes();
+
         for (Foto foto : fotos) {
             JPanel mini = new JPanel();
             mini.setLayout(new BoxLayout(mini, BoxLayout.Y_AXIS));
-            mini.add(new JLabel(new ImageIcon(foto.getCaminho())));
-            mini.add(new JLabel(foto.getLegenda()));
+            mini.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+            ImageIcon icon = new ImageIcon(foto.getCaminho());
+            if (icon.getIconWidth() <= 0) {
+                mini.add(new JLabel("[Imagem não encontrada]"));
+            } else {
+                Image img = icon.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH);
+                mini.add(new JLabel(new ImageIcon(img)));
+            }
+            JLabel legenda = new JLabel(foto.getLegenda());
+            legenda.setHorizontalAlignment(SwingConstants.CENTER);
+            mini.add(legenda);
             fotosPanel.add(mini);
         }
         add(new JScrollPane(fotosPanel), BorderLayout.CENTER);
