12e2ddae5c1f2c044dded48711b65861af9313de
Tatá <143547731+tataFaucz@users.noreply.github.com>
2025-06-11 19:46:44 -0300

    Update Main.java

--- projeto rede social/negocio/Main.java
+++ projeto rede social/negocio/Main.java
@@ -1,30 +1,39 @@
 package negocios;
 
-import dados.*;
+import dados.Foto;
+import dados.Usuario;
+import dados.Comentario;
 import ui.MainFrame;
 
 public class Main {
     public static void main(String[] args) {
         Sistema sistema = new Sistema();
-        Usuario alice = new Usuario("Alice");
-        Usuario bob = new Usuario("Bob");
-        Usuario carol = new Usuario("Carol");
-        Usuario baldo = new Usuario("Baldo");
 
-        sistema.adicionarUsuario(alice);
-        sistema.adicionarUsuario(baldo);
-        sistema.adicionarUsuario(bob);
-        sistema.adicionarUsuario(carol);
+        Usuario alice = new Usuario("Alice", "alice@email.com", "123", null);
+        Usuario bob = new Usuario("Bob", "bob@email.com", "123", null);
+        Usuario carol = new Usuario("Carol", "carol@email.com", "123", null);
+        Usuario baldo = new Usuario("Baldo", "baldo@email.com", "123", null);
 
-        sistema.adicionarAmg(alice, baldo);
-        sistema.adicionarAmg(bob, baldo);
-        sistema.adicionarAmg(alice, bob);
+        sistema.getUsuarios().add(alice);
+        sistema.getUsuarios().add(bob);
+        sistema.getUsuarios().add(carol);
+        sistema.getUsuarios().add(baldo);
 
-        Foto fotoBob = new Foto("imagem.bob.png", "Minha primeira foto!");
-        bob.adicionarFoto(fotoBob);
+        alice.seguir(baldo);
+        bob.seguir(baldo);
+        alice.seguir(bob);
 
-        Foto fotoBaldo = new Foto("imagem.baldo.png", "Lecionando!");
-        baldo.adicionarFoto(fotoBaldo);
+        Foto fotoBob = new Foto("imagem.bob.png", "Minha primeira foto!", bob);
+        bob.adicionarPublicacao(fotoBob);
+
+        Foto fotoBaldo = new Foto("imagem.baldo.png", "Minha primeira foto!", baldo);
+        baldo.adicionarPublicacao(fotoBaldo);
+
+        alice.enviarMensagem(bob, "Oi Bob, tudo bem?");
+        bob.enviarMensagem(alice, "Oi Alice! Tudo ótimo e você?");
+
+        Comentario comentario1 = new Comentario("Parabéns pelo projeto!", alice.getNome());
+        fotoBob.getComentarios().add(comentario1);
 
         javax.swing.SwingUtilities.invokeLater(() -> {
             new MainFrame(sistema).setVisible(true);
