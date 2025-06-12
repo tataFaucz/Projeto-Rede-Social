454286e22d397cc5db1835a0cc8cdbac3f036fd2
Tatá <143547731+tataFaucz@users.noreply.github.com>
2025-06-12 19:25:11 -0300

    Update MainFrame.java

--- projeto rede social/ui/MainFrame.java
+++ projeto rede social/ui/MainFrame.java
@@ -1,34 +1,42 @@
 package ui;
 
 import negocios.Sistema;
-
 import javax.swing.*;
 
 public class MainFrame extends JFrame {
     private Sistema sistema;
 
     public MainFrame(Sistema sistema) {
-        sistema = new Sistema();
+        this.sistema = sistema;
         setTitle("Rede Social");
-        setSize(800, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400, 300);
         setLocationRelativeTo(null);
-
-        mostrarLogin();
+        mostrarInicio();
     }
 
-    public void mostrarLogin() {
-        setContentPane(new LoginPanel(sistema, this));
+    public void mostrarInicio() {
+        setContentPane(new InicioPanel(sistema, this));
         revalidate();
+        repaint();
     }
 
     public void mostrarCadastro() {
         setContentPane(new CadastroPanel(sistema, this));
         revalidate();
+        repaint();
+    }
+
+    public void mostrarLogin() {
+        setContentPane(new LoginPanel(sistema, this));
+        revalidate();
+        repaint();
     }
 
     public void mostrarFeed() {
-        setContentPane(new FeedPanel(sistema, this));
-        revalidate();
+        // Troque para o painel principal do seu sistema
+        // setContentPane(new FeedPanel(sistema, this));
+        // revalidate();
+        // repaint();
     }
 }
