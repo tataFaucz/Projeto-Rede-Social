6d3f526478798184bad4f72578a3213e09ca98c4
Tatá <143547731+tataFaucz@users.noreply.github.com>
2025-06-12 20:40:05 -0300

    Update MainFrame.java

--- projeto rede social/ui/MainFrame.java
+++ projeto rede social/ui/MainFrame.java
@@ -2,16 +2,25 @@ package ui;
 
 import negocios.Sistema;
 import javax.swing.*;
+import java.awt.*;
 
 public class MainFrame extends JFrame {
     private Sistema sistema;
+
+    // Paleta de amarelos
+    public static final Color AMARELO_CLARO = new Color(255, 249, 196);
+    public static final Color AMARELO_MEDIO = new Color(255, 235, 59);
+    public static final Color AMARELO_ESCURO = new Color(255, 193, 7);
+    public static final Color AMARELO_BOTAO = new Color(255, 213, 79);
+    public static final Color AMARELO_BORDA = new Color(255, 179, 0);
 
     public MainFrame(Sistema sistema) {
         this.sistema = sistema;
         setTitle("Rede Social");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(400, 300);
+        setSize(500, 400);
         setLocationRelativeTo(null);
+        getContentPane().setBackground(AMARELO_CLARO);
         mostrarInicio();
     }
 
@@ -34,9 +43,8 @@ public class MainFrame extends JFrame {
     }
 
     public void mostrarFeed() {
-        // Troque para o painel principal do seu sistema
-        // setContentPane(new FeedPanel(sistema, this));
-        // revalidate();
-        // repaint();
+        setContentPane(new FeedPanel(sistema, this));
+        revalidate();
+        repaint();
     }
 }
